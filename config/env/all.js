bf4efa302323024ea26b116728da0921cd03f757
Nejc <nejc.silc@gmail.com>
2015-04-18 09:53:59 +0200

    ga

--- config/env/all.js
+++ config/env/all.js
@@ -53,7 +53,9 @@ module.exports = {
 				'public/lib/angular-animate/angular-animate.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/angulartics/dist/angulartics.min.js',
+				'public/lib/angulartics/dist/angulartics-ga.min.js'
 			]
 		},
 		css: [
